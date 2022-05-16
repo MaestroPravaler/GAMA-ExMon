cd381c155f89a67324e3d15a82a308e0f5b96811
RobsonMaestro <robson_maestro@alumni.usp.br>
2022-05-16 15:05:06 -0300

    Verificando os movimentos do jogador

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
-  alias ExMon.Game.Status
+  alias ExMon.Game.{Actions, Status}
 
   @computer_name "Robotinick"
   def create_player(name, move_avg, move_rnd, move_heal) do
@@ -12,5 +12,19 @@ defmodule ExMon do
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
     Status.print_round_message()
+  end
+
+  def make_move(move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> "cura"
+      move -> Actions.attack(move)
+    end
   end
 end
